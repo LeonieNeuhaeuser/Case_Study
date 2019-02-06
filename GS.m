0b1263d256a4f4833502b2b463796ee40084023b
JustinCNY <35872468+JustinCNY@users.noreply.github.com>
2019-02-06 10:55:48 +0000

    Add files via upload

--- GS.m
+++ GS.m
@@ -1,16 +1,18 @@
-function [u ,err]= GS(A,f,u0,tol)
+function [u ,err,errvec]= GS(A,f,u0,tol)
 
     n = length(u0);
     u = u0; 
     err = max(abs(A*u-f));
     temp = zeros(n,1);
     diagonal = diag(A);
+    errvec =[];
     
     while err > tol
         for i = 1:n                        
             u(i) = 1/diagonal(i)*(-dot(A(i,1:i-1),u(1:i-1))-dot(A(i,i+1:n),u(i+1:n))+f(i));
         end  
         err = max(abs(A*u-f));        
+        errvec = [errvec err];
     end
     
 
