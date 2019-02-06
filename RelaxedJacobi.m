62651e6d8d37229f6058b630aafab493d7e256a5
Leonie <leonie.neuhaeuser@gmx.de>
2019-02-06 10:51:58 +0100

    RelaxedJacobi with errorvector and GS

--- RelaxedJacobi.m
+++ RelaxedJacobi.m
@@ -1,18 +1,22 @@
-function [u,err] = RelaxedJacobi(w,A,f,u0,tol)
+function [u,err, err_vec] = RelaxedJacobi(w,A,f,u0,tol)
 
-    n = length(u0);
+    n = length(u0)
     I = eye(n);
     L = tril(A,-1); 
     R = triu(A,1); 
     D_inv = spdiags(1./diag(A),0,n,n); 
+    size(A)
+    
     
     u = u0; 
-    err = max(abs(A*u-f));
+    err = max(abs(A*u-f))
+    err_vec = []
     
     while err > tol 
         u = ((1-w).* I - w .* D_inv *(L+R))*u + w.*D_inv*f;
         err = max(abs(A*u-f));
         disp(err)
+        err_vec = [err_vec,err]
     end
 
 end
