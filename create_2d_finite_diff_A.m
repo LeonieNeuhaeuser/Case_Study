fd76fd7e282406f9892b95c662e294350cb44e69
Thom <thom.fronik@gmail.com>
2019-02-06 10:32:23 +0000

    modified the matrix creation

--- create_2d_finite_diff_A.m
+++ create_2d_finite_diff_A.m
@@ -3,9 +3,9 @@ function A=create_2d_finite_diff_A(a,b,c,d,N,M)
 %M number of pts in Y dir
 %ab is the x interval
 %cd is the y interval
-dx=abs(b-a)/(N-1);
-dy=abs(c-d)/(M-1);
-e1=repmat(-1/dy^2,(M-1)*(N-1),1);
+dx=abs(b-a)/N;
+dy=abs(c-d)/M;
+e1=  repmat(-1/dy^2,(M-1)*(N-1),1);
 e2=repmat(2*(dx^(-2)+dy^(-2)),(M-1)*(N-1),1);
 e3=repmat(-1/dx^2,(M-1)*(N-1),1);
 A = spdiags([e3 e1 e2 e1 e3 ], [-M+1,-1,0,1,M-1],(M-1)*(N-1), (M-1)*(N-1));
